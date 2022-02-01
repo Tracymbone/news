cefad7c608ca509f056f602aa6c39cbd980b3ed9
moringa <tressy.mbone@student.moringaschool.com>
2022-02-02 00:21:11 +0300

    initial changes

--- views.py
+++ views.py
@@ -1,43 +1,50 @@
-from flask import Flask, render_template
 
-from newsapi import NewsApiClient
+from flask import Flask, render_template
+import requests
+import json
+# from newsapi import NewsApiClient
 
 
 app=Flask(__name__)
 @app.route('/')
 def test():
     
-    newapi = NewsApiClient(api_key="45778e35f22c47dfadc524155cee34d6")
-    newswordhippos = newapi.get_news_wordhippos(sources="bbc-news")
+    # base_url="https://newsapi.org/v2/everything?q=bitcoin&apiKey=45778e35f22c47dfadc524155cee34d6"
+    # base_url="https://newsapi.org/v2/everything?q=Apple&from=2022-01-25&sortBy=popularity&apiKey=45778e35f22c47dfadc524155cee34d6"
+    # data=requests.get(base_url).json()["articles"]
+    # print(data)
+    return render_template("index.html")
     
-    articles = newswordhippos['articles']
+    # newswordhippos = newapi.get_news_wordhippos(sources="bbc-news")
     
-    
-    tit= []
-    description= []
-    image= []
-    content= []
-    url= []
-    publishedAt= []
+    # articles = newswordhippos['articles']
     
     
-    for i in range(len(articles)):
-        myarticles = articles[i]
+    # tit= []
+    # description= []
+    # image= []
+    # content= []
+    # url= []
+    # publishedAt= []
+    
+    
+    # for i in range(len(articles)):
+    #     myarticles = articles[i]
         
         
-        tit.append(myarticles['title'])
-        description.append(myarticles['description'])
-        image.append(myarticles['urlToImage'])
-        content.append(myarticles['content'])
-        url.append(myarticles['url'])
-        publishedAt.append(myarticles['publishedAt'])
+    #     tit.append(myarticles['title'])
+    #     description.append(myarticles['description'])
+    #     image.append(myarticles['urlToImage'])
+    #     content.append(myarticles['content'])
+    #     url.append(myarticles['url'])
+    #     publishedAt.append(myarticles['publishedAt'])
         
-        my_list = zip(tit,description,image,content)
+    #     my_list = zip(tit,description,image,content)
         
-        return render_template('index.html', context=my_list)
+    #     return render_template('index.html', context=my_list)
         
-        if __name__ == '__main__':
-         app.run(debug=True)
+    #     # if __name__ == '__main__':
+    #     #  app.run(debug=True)
         
     
     
